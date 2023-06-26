946dd3cd968bdb30be0f46c21e741c18d8eb58e4
Đỗ Ngọc Minh Tiến <tien.dnm@outlook.com>
2023-06-26 11:50:07 +0700

    add full

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
-import { submitForm } from "./server";
+import { useEffect, useState } from "react";
+import { submitForm, getFiles } from "./server";
+import Image from "next/image";
 
 export default function Home() {
+  const [hi, setHi] = useState<string[]>();
+  useEffect(() => {
+    getFiles().then((x) => {
+      setHi(x);
+    });
+  }, []);
   return (
     <>
-      <form action={submitForm}>
-        <input name="miti" />
+      <form
+        action={async (formData) => {
+          submitForm(formData).then((x) => {
+            getFiles().then((x) => {
+              setHi(x);
+            });
+          });
+        }}>
         <input
           name="file"
           type="file"
@@ -13,6 +27,22 @@ export default function Home() {
         />
         <button type="submit">submit</button>
       </form>
+      <div className="flex gap-2 flex-wrap mt-10">
+        {hi?.map((x) => {
+          return (
+            <div
+              className="w-40 h-40 relative "
+              key={x}>
+              <Image
+                alt="test"
+                src={`/upload/${x}`}
+                className="object-cover rounded-md"
+                fill
+              />
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,10 @@
 "use server";
-import { writeFileSync, mkdir } from "fs";
+import { writeFileSync, mkdir, readdir, readdirSync } from "fs";
 import path from "path";
 export async function submitForm(formData: FormData) {
   const uploadPath = path.join(process.cwd(), "public", "upload");
   const files = formData.getAll("file");
+  const res: string[] = [];
   for (const file of files) {
     if (file instanceof Blob) {
       const miti = await file.arrayBuffer();
@@ -11,7 +12,15 @@ export async function submitForm(formData: FormData) {
       const fullFile = path.join(uploadPath, file.name);
       mkdir(uploadPath, () => {
         writeFileSync(fullFile, buffer);
+        res.push(file.name);
       });
     }
   }
+  return res;
+}
+
+export async function getFiles() {
+  const uploadPath = path.join(process.cwd(), "public", "upload");
+  const dir = readdirSync(uploadPath);
+  return dir;
 }
