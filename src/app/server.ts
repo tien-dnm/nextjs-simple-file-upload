607b3d1240eb99ebabf6ff3ebca246e6d5e58972
Đỗ Ngọc Minh Tiến <tien.dnm@outlook.com>
2023-07-19 09:48:52 +0700

    try catch

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,12 +1,10 @@
 "use server";
 import {
-  writeFileSync,
-  mkdir,
-  readdir,
-  readdirSync,
   existsSync,
   mkdirSync,
+  readdirSync,
   statSync,
+  writeFileSync,
 } from "fs";
 import path from "path";
 
@@ -29,67 +27,86 @@ const createFolderWithNumber = (
   number: number,
   ...slugs: string[]
 ) => {
-  const fullPath = path.join(uploadPath, ...slugs);
-  const folderNameWithNumber = `${folderName}_${number}`;
-  const newFolderPath = path.join(fullPath, folderNameWithNumber);
-  if (folderExists(newFolderPath)) {
-    // If the folder with the number already exists, recursively call the function with an incremented number
-    createFolderWithNumber(folderName, number + 1, ...slugs);
-  } else {
-    mkdirSync(newFolderPath, { recursive: true });
-  }
+  return tryCatchWrapper(() => {
+    const fullPath = path.join(uploadPath, ...slugs);
+    const folderNameWithNumber = `${folderName}_${number}`;
+    const newFolderPath = path.join(fullPath, folderNameWithNumber);
+    if (folderExists(newFolderPath)) {
+      // If the folder with the number already exists, recursively call the function with an incremented number
+      createFolderWithNumber(folderName, number + 1, ...slugs);
+    } else {
+      mkdirSync(newFolderPath, { recursive: true });
+    }
+  });
 };
 
 export async function submitForm(formData: FormData, ...slugs: string[]) {
-  const fullPath = path.join(uploadPath, ...slugs);
-  const files = formData.getAll("file");
-  const res: string[] = [];
-  for (const file of files) {
-    if (file instanceof Blob) {
-      const miti = await file.arrayBuffer();
-      const buffer = Buffer.from(miti);
-      const fullFile = path.join(fullPath, file.name);
-      if (!existsSync(fullPath)) {
-        mkdirSync(fullPath, { recursive: true });
+  return tryCatchWrapper(async () => {
+    const fullPath = path.join(uploadPath, ...slugs);
+    const files = formData.getAll("file");
+    const res: string[] = [];
+    for (const file of files) {
+      if (file instanceof Blob) {
+        const miti = await file.arrayBuffer();
+        const buffer = Buffer.from(miti);
+        const fullFile = path.join(fullPath, file.name);
+        if (!existsSync(fullPath)) {
+          mkdirSync(fullPath, { recursive: true });
+        }
+        writeFileSync(fullFile, buffer);
+        res.push(file.name);
       }
-      writeFileSync(fullFile, buffer);
-      res.push(file.name);
     }
-  }
-  return res;
+    return res;
+  });
 }
 
 const readDirectory = (...slugs: string[]) => {
-  const fullPath = path.join(uploadPath, ...slugs);
-  let files: DirResult[] = [];
-  const items = readdirSync(fullPath);
-  for (const item of items) {
-    const itemPath = path.join(fullPath, item);
-    if (statSync(itemPath).isDirectory()) {
-      files.push({
-        path: [...slugs, item],
-        type: "folder",
-      });
-    } else {
-      files.push({
-        path: [...slugs, item],
-        type: "image",
-      });
+  return tryCatchWrapper(() => {
+    const fullPath = path.join(uploadPath, ...slugs);
+    let files: DirResult[] = [];
+    const items = readdirSync(fullPath);
+    for (const item of items) {
+      const itemPath = path.join(fullPath, item);
+      if (statSync(itemPath).isDirectory()) {
+        files.push({
+          path: [...slugs, item],
+          type: "folder",
+        });
+      } else {
+        files.push({
+          path: [...slugs, item],
+          type: "image",
+        });
+      }
     }
-  }
-  return files;
+    return files;
+  });
 };
 
 export async function getFiles(...slugs: string[]) {
-  const fullPath = path.join(uploadPath, ...slugs);
-  const exist = existsSync(fullPath);
-  if (exist) {
-    const dir = readDirectory(...slugs);
-    return dir;
-  }
-  return [];
+  return tryCatchWrapper(() => {
+    const fullPath = path.join(uploadPath, ...slugs);
+    const exist = existsSync(fullPath);
+    if (exist) {
+      const dir = readDirectory(...slugs);
+      return dir;
+    }
+    return [];
+  });
 }
 
 export async function newFolder(...slugs: string[]) {
-  createFolderWithNumber("New Folder", 1, ...slugs);
+  return tryCatchWrapper(() => {
+    return createFolderWithNumber("New Folder", 1, ...slugs);
+  });
+}
+
+function tryCatchWrapper<T>(func: () => T): T | undefined {
+  try {
+    return func();
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return undefined;
+  }
 }
